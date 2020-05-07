93462e0f9796bf8fd8ac465bf30e8518bbf6c17e
Vinh Nguyen <vinh.nguyen@sjsu.edu>
2020-05-07 15:16:46 -0700

    Included unit tests and removed virtual env

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from app_folder.models import User
 def concat_lists(list_A, list_B):
     return list_A + list_B
+
+def create_new_user(username,email,password):
+
+	user = User(username = username,email = email)
+	user.set_password(password)
+
+	return user
--- test_main.py
+++ test_main.py
@@ -1,7 +1,20 @@
 import main
+from app_folder.models import User
 
 def test_main():
     # set up test
     a = [1,2,3]
     b = [4,5]
     assert main.concat_lists(a,b) == [1,2,3,4,5]
+
+def test_user():
+
+	username = "bobross"
+	email = "bobross@gmail.com"
+	password = "bobrosspainting"
+
+	new_user = main.create_new_user(username,email,password)
+
+	assert new_user.username == "bobross"
+	assert new_user.email == "bobross@gmail.com"
+	assert new_user.check_password(password) == True
